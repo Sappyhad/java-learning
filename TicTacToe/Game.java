bb5efcf25da015090ecc2bb3b6d225791faa7664
Sappyhad <jakub.bylinski@gmail.com>
2024-04-10 09:49:08 +0200

    fixed bugs on tictactoe winner check

--- TicTacToe/Game.java
+++ TicTacToe/Game.java
@@ -70,19 +70,39 @@ public class Game {
     private Player getWinner() {
         // Check if there is a winner
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+
+            // 1st row
+            if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][2] != ' '){
                 return players[previousPlayerIndex];
             }
-            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+            // 2nd row
+            if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][2] != ' '){
                 return players[previousPlayerIndex];
             }
-            if (board[i][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != ' ' && board[i][0] != ' ') {
+            // 3rd row
+            if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][2] != ' '){
                 return players[previousPlayerIndex];
             }
-            if (board[i][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ' && board[0][2] != ' ' && board[i][0] != ' ') {
+
+            // 1st col
+            if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[2][0] != ' '){
                 return players[previousPlayerIndex];
             }
-            if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+            // 2nd col
+            if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[2][1] != ' '){
+                return players[previousPlayerIndex];
+            }
+            // 3rd col
+            if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[2][2] != ' '){
+                return players[previousPlayerIndex];
+            }
+
+            // cross - left to right
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != ' '){
+                return players[previousPlayerIndex];
+            }
+            // cross - right to left
+            if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != ' '){
                 return players[previousPlayerIndex];
             }
         }
